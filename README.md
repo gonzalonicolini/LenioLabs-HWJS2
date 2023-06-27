2b166bf3dc9e3f9ea72cbe86a67e811af2ee01f0
Eduardo Brito <eduardobrito@Eduardos-MacBook-Pro.local>
2023-06-26 21:23:12 -0300

    Feat: add functions and timers homework

---
+++ README.md
@@ -0,0 +1,7 @@
+# Homework JavaScript
+
+## Functions
+
+1. Given an array of Marvel characters, create a function that sorts them by release date.
+
+2. Create a function that logs in the terminal numbers from 1 to 10 in a given interval. For example if the interval is 2000, it means that every 2s `console.log(1)` then `console.log(2)` and so on.
---
+++ functions.js
@@ -0,0 +1,30 @@
+const superheros = [
+    {
+        name: 'IronMan',
+        date: '03/01/1963'
+    },
+    {
+        name: 'SpiderMan',
+        date: '03/07/1961'
+    },
+    {
+        name: 'Captain America',
+        date: '03/08/1941'
+    },
+    {
+        name: 'Doctor Strange',
+        date: '07/20/1963'
+    },
+    {
+        name: 'Shang-Chi',
+        date: '12/05/1973'
+    }
+]
+
+function getSortedSuperheros(heros) {
+    // your code goes here
+}
+
+function specialLogs(interval) {
+    // your code goes here
+}
